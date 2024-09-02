f7b159c082fec9155c9928ec83bf93863788d848
sergiovillaa <122813731+sergiovillaa@users.noreply.github.com>
2024-09-02 08:44:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ st.markdown(":violet[El **DataFrame** es el siguiente:]")
 
 #----- Renderizado del DataFrame ----------------------------------
 st.dataframe(datos_df)
-st.markdown(vars_per[0])
+st.markdown(ganan_selected)
 st.divider()
 
 
